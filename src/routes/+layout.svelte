5bb2149147a239999bff5feeeb3a6455abca1d57
Jeremy <010JGL@GMAIL.COM>
2023-06-01 20:13:25 -0400

    routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/gallery">Gallery</a>
+  <a href="/nfts/[slug]">NFTs</a>
+  <a href="/collection">Collection</a>
+  <a href="/upload">Upload</a>
+</nav>
+
+<slot></slot>
